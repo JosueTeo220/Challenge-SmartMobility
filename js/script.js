5004d5abbbd369e0bcb0c404c1e6a9b316ec132c
JosueTeo220 <josueteobaldo220@hotmail.com>
2022-11-20 09:16:55 -0300

    modified

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
-window.addEventListener('scroll', ()=>{
-    
+const scrolledElement = document.querySelectorAll('.scrollView');
+console.log('ola')
+
+const elementoNaTela = (element, dividendo = 1) => {
+    const elementoTop = element.getBoundingClientRect().top;
+    return  (
+        elementoTop <= (window.innerHeight || document.documentElement.clientHeight) / dividendo
+    );
+}
+const elementoForDaTela = (element) => {
+    const elementoTop = element.getBoundingClientRect().top;
+    return  (
+        elementoTop > (window.innerHeight || document.documentElement.clientHeight)
+    );
+}
+const displayElemento = (elemento) => {
+    elemento.classList.add('scrolled');
+}
+const hideElemento = (elemento) => {
+    elemento.classList.remove('scrolled');
+}
+const animateScroll = () =>{
+    scrolledElement.forEach((element) =>{
+        if(elementoNaTela(element, 1.25)){
+            console.log('elementoNaTela');
+            displayElemento(element);
+        }
+        else if(elementoForDaTela(element)){
+            console.log('elementoForaTela')
+            hideElemento(element);
+        }
+    })
+}
+window.addEventListener("scroll", ()=>{
+    animateScroll();
 })
