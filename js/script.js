521467f4e36612da6d8f5871f473cd51a01b8e42
JosueTeo220 <josueteobaldo220@hotmail.com>
2022-11-21 14:41:32 -0300

    modified

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-const scrolledElement = document.querySelectorAll('.scrollView');
 console.log('test DOM')
+var control = 0
+const scrolledElement = document.querySelectorAll('.scrollView');
+const menuLink = document.querySelectorAll('.menuLink')
+menuLink.forEach(hover => {
+    console.log('entrou')
+    hover.addEventListener('mouseenter', () => {
+        console.log('entrou')
+        hover.classList.add('menuEnter')
+        
+    });
+    hover.addEventListener('mouseleave', () => {
+            console.log('saiu')
+            hover.classList.remove('menuEnter')
+    })
+    
+})
+
+
 
 const elementoNaTela = (element, dividendo = 1) => {
     const elementoTop = element.getBoundingClientRect().top;
